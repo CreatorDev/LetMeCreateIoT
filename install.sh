8049a3e9dff89de46c98ed29f8d3e1114d1d11c6
Michal Tusnio <m.tusnio@gmail.com>
2016-07-12 20:32:44 +0100

    Install: Added

---
+++ install.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+if [[ $# -ne 1 ]]; then
+    echo "Usage: $0 contiki_dir"
+    exit 1
+fi
+
+echo $1
+CONTIKI=$(readlink -f $1)
+
+echo "Installing the library files to $CONTIKI"
+
+LIBRARY_DIR="core"
+LMC_DIR="$CONTIKI/$LIBRARY_DIR/lmc"
+SRC_DIR="src"
+INCLUDE_DIR="include"
+
+if [[ ! -d "$CONTIKI/$LIBRARY_DIR" ]]; then
+    echo "Could not find directory $LIBRARY_DIR. Check your contiki path"
+    exit 1
+fi
+
+if [[ -d $LMC_DIR ]]; then
+    rm -rf $LMC_DIR
+fi
+
+mkdir -p $LMC_DIR
+
+cp -r ./$SRC_DIR/* $LMC_DIR/
+cp -r ./$INCLUDE_DIR/* $LMC_DIR/
+
+echo "Modifying Contiki makefile..."
+
+sed -i '0,/MODULES +=/s//MODULES += core\/lmc\/core/' $CONTIKI/Makefile.include
+
+if [[ $? -ne 0 ]]; then
+    echo "Failed to modify the makefile"
+    exit 1
+fi
+
+echo "Library installed successfully"
