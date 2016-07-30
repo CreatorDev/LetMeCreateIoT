1e3e1391eed21d1afe41fd61e0406aab9c4e16a5
Michal Tusnio <m.tusnio@gmail.com>
2016-07-30 14:45:47 +0100

    Relay2: Added support

---
+++ src/click/relay2.c
@@ -0,0 +1,106 @@
+#include <stdio.h>
+
+#ifndef CONTIKI
+#include "core/common.h"
+#endif
+
+#include <letmecreate/core/gpio.h>
+#include <letmecreate/click/relay2.h>
+
+
+int relay2_click_enable_relay_1(uint8_t mikrobus_index)
+{
+#ifndef CONTIKI
+    switch (mikrobus_index) {
+    case MIKROBUS_1:
+#endif
+        if (!gpio_init(MIKROBUS_1_PWM)
+        &&  !gpio_set_direction(MIKROBUS_1_PWM, GPIO_OUTPUT)
+        &&  !gpio_set_value(MIKROBUS_1_PWM, 1))
+            return 0;
+#ifndef CONTIKI
+    case MIKROBUS_2:
+        if (!gpio_init(MIKROBUS_2_PWM)
+        &&  !gpio_set_direction(MIKROBUS_2_PWM, GPIO_OUTPUT)
+        &&  !gpio_set_value(MIKROBUS_2_PWM, 1))
+            return 0;
+        default:
+            fprintf(stderr, "relay2: Invalid mikrobus index.\n");
+            return -1;
+    }
+#endif
+    return -1;
+}
+
+int relay2_click_disable_relay_1(uint8_t mikrobus_index)
+{
+#ifndef CONTIKI
+    switch (mikrobus_index) {
+    case MIKROBUS_1:
+#endif
+        if (!gpio_init(MIKROBUS_1_PWM)
+        &&  !gpio_set_direction(MIKROBUS_1_PWM, GPIO_OUTPUT)
+        &&  !gpio_set_value(MIKROBUS_1_PWM, 0))
+            return 0;
+#ifndef CONTIKI
+    case MIKROBUS_2:
+        if (!gpio_init(MIKROBUS_2_PWM)
+        &&  !gpio_set_direction(MIKROBUS_2_PWM, GPIO_OUTPUT)
+        &&  !gpio_set_value(MIKROBUS_2_PWM, 0))
+            return 0;
+        default:
+            fprintf(stderr, "relay2: Invalid mikrobus index.\n");
+            return -1;
+    }
+#endif
+    return -1;
+}
+
+int relay2_click_enable_relay_2(uint8_t mikrobus_index)
+{
+#ifndef CONTIKI
+    switch (mikrobus_index) {
+    case MIKROBUS_1:
+#endif
+        if (!gpio_init(MIKROBUS_1_AN)
+        &&  !gpio_set_direction(MIKROBUS_1_AN, GPIO_OUTPUT)
+        &&  !gpio_set_value(MIKROBUS_1_AN, 1))
+             return 0;
+#ifndef CONTIKI
+    case MIKROBUS_2:
+        if (!gpio_init(MIKROBUS_2_AN)
+        &&  !gpio_set_direction(MIKROBUS_2_AN, GPIO_OUTPUT)
+        &&  !gpio_set_value(MIKROBUS_2_AN, 1))
+            return 0;
+        default:
+            fprintf(stderr, "relay2: Invalid mikrobus index.\n");
+            return -1;
+    }
+#endif
+
+    return -1;
+}
+
+int relay2_click_disable_relay_2(uint8_t mikrobus_index)
+{
+#ifndef CONTIKI
+    switch (mikrobus_index) {
+    case MIKROBUS_1:
+#endif
+        if (!gpio_init(MIKROBUS_1_AN)
+        &&  !gpio_set_direction(MIKROBUS_1_AN, GPIO_OUTPUT)
+        &&  !gpio_set_value(MIKROBUS_1_AN, 0))
+            return 0;
+#ifndef CONTIKI
+    case MIKROBUS_2:
+        if (!gpio_init(MIKROBUS_2_AN)
+        &&  !gpio_set_direction(MIKROBUS_2_AN, GPIO_OUTPUT)
+        &&  !gpio_set_value(MIKROBUS_2_AN, 0))
+            return 0;
+        default:
+            fprintf(stderr, "relay2: Invalid mikrobus index.\n");
+            return -1;
+    }
+#endif
+    return -1;
+}
