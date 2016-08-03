7cc75e67324d1c5086a5152e9994826557ed7f56
Michal Tusnio <m.tusnio@gmail.com>
2016-08-03 11:35:49 +0100

    motion: Example added

---
+++ examples/motion/main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+#include <contiki.h>
+
+#include <sys/clock.h>
+
+#include "dev/leds.h"
+
+#include <letmecreate/click/motion.h>
+#include <letmecreate/core/interrupts.h>
+#include <letmecreate/core/common.h>
+
+void motion_callback(uint8_t event)
+{
+    leds_toggle(LED1);
+}
+
+PROCESS(main_process, "Main process");
+AUTOSTART_PROCESSES(&main_process);
+/*---------------------------------------------------------------------------*/
+PROCESS_THREAD(main_process, ev, data)
+{
+    PROCESS_BEGIN();
+
+    {
+        printf("=====Start=====\n");
+
+        motion_click_enable(MIKROBUS_1);
+
+        motion_click_attach_callback(MIKROBUS_1, motion_callback);
+        while(1)
+        {
+        }
+
+        motion_click_disable(MIKROBUS_1);
+    }
+
+    PROCESS_END();
+}
+
+/*---------------------------------------------------------------------------*/
