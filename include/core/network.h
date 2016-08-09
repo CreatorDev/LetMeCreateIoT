1e35360e37f443af1d94af4e4a1b793b4dc64540
Michal Tusnio <m.tusnio@gmail.com>
2016-08-09 16:48:30 +0100

    Network: Expanded with send/receive functions

--- include/core/network.h
+++ include/core/network.h
@@ -9,8 +9,13 @@
 #define H_CORE_NETWORK
 
 #include <stdint.h>
+#include <contiki-net.h>
 
-struct uip_udp_conn;
+/** Halts the process until UDP data has been sent */
+#define PROCESS_WAIT_UDP_SENT() PROCESS_WAIT_EVENT_UNTIL(ev == tcpip_event)
+
+/** Halts the process until UDP data is ready to be received */
+#define PROCESS_WAIT_UDP_RECEIVED() PROCESS_WAIT_EVENT_UNTIL(ev == tcpip_event && uip_newdata())
 
 /**
  * @brief Sets local IPv6 address
@@ -33,7 +38,43 @@ int set_ipv6_address(const char * address, uip_ipaddr_t * addr);
  */
 struct uip_udp_conn * udp_new_connection(uint16_t local_port, uint16_t remote_port, const char * remote_address);
 
+/**
+ * @brief Sends data over a UDP connection.
+ *
+ * Uses the IP provided in the connection structure to send a buffer of data
+ *
+ * @param[in] connection UDP connection
+ * @param[in] data Data buffer
+ * @param[in] len Length of the data to be sent
+ * @return 0 if successful, -1 if failed
+ */
+int udp_packet_send(struct uip_udp_conn * connection, const uint8_t * data, uint32_t len);
 
+/**
+ * @brief Sends data over a UDP connection to one specific client.
+ *
+ * Sends a packet of data to one client as specified by the address
+ *
+ * @param[in] connection UDP connection
+ * @param[in] data Data buffer
+ * @param[in] len Length of the data to be sent
+ * @param[in] address String containing the client's IP address
+ * @param[in] port Remote port to send to
+ * @return 0 if successful, -1 if failed
+ */
+int udp_packet_sendto(struct uip_udp_conn * connection, const uint8_t * data, uint32_t len,
+    const char * address, uint16_t port);
 
+/**
+ * @brief Receives a UDP packet.
+ *
+ * Checks for and receives data available in the UDP buffer
+ *
+ * @param[out] data Data buffer to write to
+ * @param[in] len Length of the data buffer
+ * @param[out] packet_data Optional, structure containing metadata for the packet
+ * @return 0 if no data available, length of the received data if found, -1 if failed
+ */
+int udp_packet_receive(uint8_t * data, uint32_t len, struct uip_ip_hdr * packet_data);
 
 #endif
--- src/core/network.c
+++ src/core/network.c
@@ -1,9 +1,12 @@
+#include "network.h"
+
 #include <contiki.h>
 #include <contiki-net.h>
 
 #include <stdio.h>
+#include <string.h>
 
-#include "network.h"
+#define UIP_IP_BUF ((struct uip_ip_hdr *)&uip_buf[UIP_LLH_LEN])
 
 int set_ipv6_address(const char * address, uip_ipaddr_t * addr)
 {
@@ -56,3 +59,81 @@ struct uip_udp_conn * udp_new_connection(uint16_t local_port, uint16_t remote_po
 
     return conn;
 }
+
+int udp_packet_send(struct uip_udp_conn * connection, const uint8_t * data, uint32_t len)
+{
+    if(connection == NULL)
+    {
+        fprintf(stderr, "UDP: Connection cannot be null\n");
+        return -1;
+    }
+
+    if(data == NULL)
+    {
+        fprintf(stderr, "UDP: Cannot send null data\n");
+        return -1;
+    }
+
+    uip_udp_packet_send(connection, data, len);
+    tcpip_poll_udp(connection);
+
+    return 0;
+}
+
+int udp_packet_sendto(struct uip_udp_conn * connection, const uint8_t * data, uint32_t len,
+    const char * address, uint16_t port)
+{
+    uip_ipaddr_t ipaddr;
+    if(connection == NULL)
+    {
+        fprintf(stderr, "UDP: Connection cannot be null\n");
+        return -1;
+    }
+
+    if(data == NULL)
+    {
+        fprintf(stderr, "UDP: Cannot send null data\n");
+        return -1;
+    }
+
+    if(!uiplib_ipaddrconv(address, &ipaddr))
+    {
+        printf("Network: Failed to convert IP: %s\n", address);
+        return -1;
+    }
+
+
+    uip_udp_packet_sendto(connection, data, len, &ipaddr, port);
+    tcpip_poll_udp(connection);
+
+    return 0;
+}
+
+int udp_packet_receive(uint8_t * data, uint32_t len, struct uip_ip_hdr * packet_data)
+{
+    if(data == NULL)
+    {
+        fprintf(stderr, "UDP: Receive data cannot be null\n");
+        return -1;
+    }
+
+    if(uip_newdata())
+    {
+        if(len > uip_datalen())
+        {
+            len = uip_datalen();
+        }
+
+        memcpy(data, (uint8_t*)uip_appdata, len);
+
+        if(packet_data)
+        {
+           memcpy(packet_data, UIP_IP_BUF, sizeof(struct uip_ip_hdr));
+        }
+
+        return len;
+    }
+
+    return 0;
+}
+
