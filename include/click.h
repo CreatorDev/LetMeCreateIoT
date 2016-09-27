fc2abda9f340646acbf51870077b16af42302823
Michal Tusnio <m.tusnio@gmail.com>
2016-09-27 10:31:09 +0100

    Click: Header updated

--- include/click.h
+++ include/click.h
@@ -9,13 +9,13 @@
 #ifndef H_CLICK
 #define H_CLICK
 
-#include "letmecreate/3rd_party/bme280.h"
-
 #include "letmecreate/click/accel.h"
 #include "letmecreate/click/air_quality.h"
+#include "letmecreate/click/alcohol.h"
 #include "letmecreate/click/color.h"
 #include "letmecreate/click/common.h"
 #include "letmecreate/click/co.h"
+#include "letmecreate/click/ir_eclipse.h"
 #include "letmecreate/click/joystick.h"
 #include "letmecreate/click/led_matrix.h"
 #include "letmecreate/click/motion.h"
