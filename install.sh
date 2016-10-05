760ce00cc4e2de3534908e3fc0a257c494ae2a14
Michal Tusnio <m.tusnio@gmail.com>
2016-10-05 16:12:35 +0100

    Install: Fixed wrong return code check, changed grep -P to -E

--- install.sh
+++ install.sh
@@ -30,7 +30,7 @@ function install_files {
 
     MAKEFILE=$CONTIKI/Makefile.include
 
-    grep --quiet -P "MODULES(.*?)$LIBRARY_DIR_NAME/$LMC_DIR_NAME/core" $MAKEFILE
+    grep -E --quiet "MODULES(.*?)$LIBRARY_DIR_NAME/$LMC_DIR_NAME/core" $MAKEFILE
     if [[ $? -eq 1 ]]; then
         echo "Adding LMC core module to Contiki makefile..."
         sed -i "0,/MODULES +=/s//MODULES += ${LIBRARY_DIR_NAME}\/${LMC_DIR_NAME}\/core/" $MAKEFILE
@@ -43,8 +43,8 @@ function install_files {
         exit 1
     fi
 
-    grep --quiet -P "MODULES(.*?)$LIBRARY_DIR_NAME/$LMC_DIR_NAME/click" $MAKEFILE
-    if [[ $? -eq 0 ]]; then
+    grep -E --quiet "MODULES(.*?)$LIBRARY_DIR_NAME/$LMC_DIR_NAME/click" $MAKEFILE
+    if [[ $? -eq 1 ]]; then
         echo "Adding LMC click module to Contiki makefile..."
         sed -i "0,/MODULES +=/s//MODULES += ${LIBRARY_DIR_NAME}\/${LMC_DIR_NAME}\/click/" $MAKEFILE
         if [[ $? -ne 0 ]]; then
