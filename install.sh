1bb09f91dd0398f081908e2115905d7b25adba8c
Michal Tusnio <m.tusnio@gmail.com>
2016-10-09 21:49:37 +0100

    Install: Fix for in-place edit compatibility between BSD/GNU sed

--- install.sh
+++ install.sh
@@ -34,11 +34,13 @@ function install_files {
     grep -E --quiet "MODULES(.*?)$LIBRARY_DIR_NAME/$LMC_DIR_NAME/core" $MAKEFILE
     if [[ $? -eq 1 ]]; then
         echo "Adding LMC core module to Contiki makefile..."
-        sed -i -e "0,/MODULES +=/s//MODULES += ${LIBRARY_DIR_NAME}\/${LMC_DIR_NAME}\/core/" $MAKEFILE
+        sed -e "0,/MODULES +=/s//MODULES += ${LIBRARY_DIR_NAME}\/${LMC_DIR_NAME}\/core/" $MAKEFILE > $MAKEFILE.new
         if [[ $? -ne 0 ]]; then
             echo "Sed failed"
+            rm $MAKEFILE.new
             return 1
         fi
+        mv $MAKEFILE.new $MAKEFILE
     elif [[ $? -gt 1 ]]; then
         echo "Grep failed"
         exit 1
@@ -47,11 +49,13 @@ function install_files {
     grep -E --quiet "MODULES(.*?)$LIBRARY_DIR_NAME/$LMC_DIR_NAME/click" $MAKEFILE
     if [[ $? -eq 1 ]]; then
         echo "Adding LMC click module to Contiki makefile..."
-        sed -i -e "0,/MODULES +=/s//MODULES += ${LIBRARY_DIR_NAME}\/${LMC_DIR_NAME}\/click/" $MAKEFILE
+        sed -e "0,/MODULES +=/s//MODULES += ${LIBRARY_DIR_NAME}\/${LMC_DIR_NAME}\/click/" $MAKEFILE > $MAKEFILE.new
         if [[ $? -ne 0 ]]; then
             echo "Sed failed"
+            rm $MAKEFILE.new
             return 1
         fi
+        mv $MAKEFILE.new $MAKEFILE
     elif [[ $? -gt 1 ]]; then
         echo "Grep failed"
         exit 1
