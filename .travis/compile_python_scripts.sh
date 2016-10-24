12a62b8cb7540d4297252b9d979c598612397375
Michal Tusnio <m.tusnio@gmail.com>
2016-10-24 15:30:12 +0100

    Travis: Updated python compile script with messages

--- .travis/compile_python_scripts.sh
+++ .travis/compile_python_scripts.sh
@@ -2,7 +2,8 @@
 
 BASE_DIR=$(dirname $(readlink -f $0))/..
 
-find $BASE_DIR/ -wholename "*.py" | xargs python3 -m py_compile
+find $BASE_DIR/ -wholename "*.py" | \
+xargs -I {} sh -c "echo 'Compiling {}' && python3 -m py_compile {} && echo 'Compiled {}'"
 
 if [[ $? -ne 0 ]]; then
     echo "Compilation of at least one script failed"
