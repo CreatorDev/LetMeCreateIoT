ab160e055f28314551100d3b6e71105a230262a9
Michal Tusnio <m.tusnio@gmail.com>
2017-01-17 14:53:42 +0000

    SPI: Made SPI transfer use the new SPI transfer duplex mode

--- src/core/spi.c
+++ src/core/spi.c
@@ -9,6 +9,8 @@
 #include <pic32_spi.h>
 #include <pic32_gpio.h>
 
+#include "letmecreate/core/gpio.h"
+
 #define DEFAULT_BAUD_RATE       (1000000)
 
 #define SPI_READ_BIT            (0x80)
@@ -31,16 +33,16 @@ int spi_init(uint8_t mode)
         flags |= SPI_CLOCK_IDLE_HIGH;
         break;
         default:
-        printf("SPI: Invalid mode\n");
+        fprintf(stderr, "SPI: Invalid mode\n");
         return -1;
     }
 
     if(pic32_spi1_init(DEFAULT_BAUD_RATE, flags))
         return -1;
 
-    GPIO_CONFIGURE_AS_DIGITAL(E, 5);
-    GPIO_CONFIGURE_AS_OUTPUT(E, 5);
-    GPIO_SET(E, 5);
+    gpio_init(GPIO_CS);
+    gpio_set_direction(GPIO_CS, GPIO_OUTPUT);
+    gpio_set_value(GPIO_CS, 1);
 
     return 0;
 }
@@ -54,93 +56,26 @@ int spi_set_speed(uint32_t speed)
 
 static int spi_start_transfer()
 {
-    GPIO_CLR(E, 5);
-
-    return 0;
+    return gpio_set_value(GPIO_CS, 0);
 }
 
 static int spi_end_transfer()
 {
-    GPIO_SET(E, 5);
-
-    return 0;
+    return gpio_set_value(GPIO_CS, 1);
 }
-
-static int spi_write(const uint8_t * buffer, uint32_t len)
-{
-    if(!buffer || len == 0)
-    {
-        printf("SPI: No data to write\n");
-        return -1;
-    }
-
-    if(pic32_spi1_write(buffer, len))
-    {
-        printf("SPI: Failed to write\n");
-        return -1;
-    }
-
-    return 0;
-}
-
-static int spi_read_registers(uint8_t reg_address, uint8_t * buffer, uint32_t len)
-{
-    if(!buffer || len == 0)
-    {
-        printf("SPI: No data to read\n");
-        return -1;
-    }
-
-    reg_address |= SPI_READ_BIT;
-    if(len > 1)
-        reg_address |= SPI_MULTIPLE_BYTE_BIT;
-
-    if(pic32_spi1_write(&reg_address, 1))
-    {
-        printf("SPI: Failed to write registry address\n");
-        return -1;
-    }
-
-    if(pic32_spi1_read(buffer, len))
-    {
-        printf("SPI: Failed to read data\n");
-        return -1;
-    }
-
-
-    return 0;
-}
-
 
 int spi_transfer(const uint8_t * tx_buffer, uint8_t * rx_buffer, uint32_t len)
 {
     if((!tx_buffer && !rx_buffer) || len == 0)
     {
-        printf("SPI: No data to write\n");
+        fprintf(stderr, "SPI: No data to transfer\n");
         return -1;
     }
 
     spi_start_transfer();
-    if(tx_buffer && !rx_buffer)
-    {
-        if(spi_write(tx_buffer, len) < 0)
-        {
-           spi_end_transfer();
-           return -1;
-        }
-    }
-    else if(!tx_buffer && rx_buffer)
-    {
-        if(spi_read_registers(tx_buffer[0], &rx_buffer[1], len - 1) < 0)
-        {
-            spi_end_transfer();
-            return -1;
-        }
-    }
-    else
+    if(pic32_spi1_transfer(tx_buffer, rx_buffer, len))
     {
         spi_end_transfer();
-        printf("SPI: Unsupported transfer case\n");
         return -1;
     }
     spi_end_transfer();
