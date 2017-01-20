20f2ea2d88791b42c5510626565ec7fd7fbbc1b9
Michal Tusnio <m.tusnio@gmail.com>
2017-01-20 13:04:59 +0000

    export.h: Added

---
+++ include/click/export.h
@@ -0,0 +1,18 @@
+/**
+ * @file export.h
+ * @author Michal Tusnio
+ * @date 2016
+ * @copyright 3-clause BSD
+ */
+
+
+#ifndef H_EXPORT
+#define H_EXPORT
+
+
+/* Those defines are only for LMC & LMCIoT compatibility which helps us get rid of extra patches
+   that just remove export.h & export macros */
+
+#define LETMECREATE_CLICK_EXPORT
+
+#endif
