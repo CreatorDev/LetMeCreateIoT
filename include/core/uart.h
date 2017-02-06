57fda48fd8d39201f9932474d27ccb3f74df6123
Francois Berder <francois.berder@imgtec.com>
2017-02-06 17:02:08 +0000

    uart: Make uart_send/uart_receive more consistent with LetMeCreate
    In LetMeCreate, uart_send/uart_receive return the number of bytes
    sent/received.
    
    Signed-off-by: Francois Berder <francois.berder@imgtec.com>

--- include/core/uart.h
+++ include/core/uart.h
@@ -77,7 +77,7 @@ int uart_get_baudrate(uint32_t *baudrate);
  *
  * @param[in] buffer Array of bytes
  * @param[in] count Number of bytes to send
- * @return 0 if successful, -1 otherwise
+ * @return @p count if successful, -1 otherwise
  */
 int uart_send(const uint8_t *buffer, uint32_t count);
 
@@ -86,7 +86,7 @@ int uart_send(const uint8_t *buffer, uint32_t count);
  *
  * @param[out] buffer Array of bytes
  * @param[in] count Number of bytes to receive
- * @return 0 if successful, -1 otherwise
+ * @return @p count if successful, -1 otherwise
  */
 int uart_receive(uint8_t *buffer, uint32_t count);
 
