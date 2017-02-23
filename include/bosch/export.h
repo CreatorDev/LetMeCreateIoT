57f1e032745b0924547b31bb55eac2e535bbc725
Michal Tusnio <m.tusnio@gmail.com>
2017-02-23 15:52:48 +0000

    bosch: add export file

---
+++ include/bosch/export.h
@@ -0,0 +1,19 @@
+/**
+ * @file bosch/export.h
+ * @author Michal Tusnio
+ * @date 2016
+ * @copyright 3-clause BSD
+ */
+
+
+#ifndef H_BOSCH_EXPORT
+#define H_BOSCH_EXPORT
+
+
+/* Those defines are only for LMC & LMCIoT compatibility which helps us get rid of extra patches
+   that just remove export.h & export macros */
+
+#define LETMECREATE_BOSCH_EXPORT
+
+#endif
+
