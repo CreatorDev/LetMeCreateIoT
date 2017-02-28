6535911195c93c7f428a67aae9c8865d215e762b
Michal Tusnio <m.tusnio@gmail.com>
2017-02-28 15:42:22 +0000

    travis: support for LMC app testing

--- .travis/check_clicks_header.sh
+++ .travis/check_clicks_header.sh
@@ -2,13 +2,18 @@
 
 BASE_DIR=$(dirname $(readlink -f $0))
 
-CLICKS=$(ls -I "ft800_defs.h" -I "common.h" -I "export.h" "$BASE_DIR/../include/click/")
+CLICKS=$(ls -I "ft800_defs.h" -I "common.h" -I "export.h" "$BASE_DIR/../letmecreate/click/")
+
+if [[ $? -ne 0 ]]; then
+    echo "Failed to look through directory"
+    exit 1
+fi
 
 echo "Checking if clicks are included in the header"
 CODE=0
 for x in $CLICKS; do
     echo "Checking if click.h includes $x"
-    grep -E --quiet "$x" "$BASE_DIR/../include/click.h"
+    grep -E --quiet "$x" "$BASE_DIR/../letmecreate/click.h"
     if [[ $? -ne 0 ]]; then
         CODE=1
         echo "Click.h file does not include $x file"
--- .travis/check_core_header.sh
+++ .travis/check_core_header.sh
@@ -2,13 +2,18 @@
 
 BASE_DIR=$(dirname $(readlink -f $0))
 
-HEADERS=$(ls "$BASE_DIR/../include/core/")
+HEADERS=$(ls "$BASE_DIR/../letmecreate/core/")
+
+if [[ $? -ne 0 ]]; then
+    echo "Failed to look through directory"
+    exit 1
+fi
 
 echo "Checking if all core headers are included in the header"
 CODE=0
 for x in $HEADERS; do
     echo "Checking if core.h includes $x"
-    grep -E --quiet "$x" "$BASE_DIR/../include/core.h"
+    grep -E --quiet "$x" "$BASE_DIR/../letmecreate/core.h"
     if [[ $? -ne 0 ]]; then
         CODE=1
         echo "Missing!"
